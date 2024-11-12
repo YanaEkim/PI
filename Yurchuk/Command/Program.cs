e46bb5edc4c0f8cfd976c56bfba28d4895cefcc9
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-11-12 13:20:56 +1000

    LR8 - fix() - Добавление яркости телевизора

--- Yurchuk/Command/Program.cs
+++ Yurchuk/Command/Program.cs
@@ -10,12 +10,37 @@ public interface ICommand
 public class Television
 {
     public int Volume { get; private set; }
+	public int Light { get; private set; }
 
-    public void SetVolume(int volume)
+	public void SetVolume(int volume)
     {
         Volume = volume;
         Console.WriteLine($"Громкость установлена на {Volume}");
     }
+	public void SetLight(int light)
+	{
+		Light = light;
+		Console.WriteLine($"Яркость установлена на {Light}");
+	}
+
+}
+
+// Команда для установки яркости телевизора
+public class SetLightCommand : ICommand
+{
+	private readonly Television _television;
+	private readonly int _light;
+
+	public SetLightCommand(Television television, int light)
+	{
+		_television = television;
+		_light = light;
+	}
+
+	public void Execute()
+	{
+		_television.SetLight(_light);
+	}
 }
 
 // Команда для установки громкости
@@ -59,16 +84,17 @@ public class Program
     {
         // Создаем объект телевизора
         var television = new Television();
+		var remoteControl = new RemoteControl();
 
-        // Запрашиваем у пользователя уровень громкости
-        Console.Write("Введите уровень громкости (0-100): ");
+		// Запрашиваем у пользователя уровень громкости
+		Console.Write("Введите уровень громкости (0-100): ");
         if (int.TryParse(Console.ReadLine(), out int volume) && volume >= 0 && volume <= 100)
         {
             // Создаем команду для изменения громкости
             var setVolumeCommand = new SetVolumeCommand(television, volume);
 
             // Создаем пульт и назначаем команду
-            var remoteControl = new RemoteControl();
+            
             remoteControl.SetCommand(setVolumeCommand);
 
             // Выполняем команду
@@ -78,5 +104,23 @@ public class Program
         {
             Console.WriteLine("Некорректный ввод. Пожалуйста, введите число от 0 до 100.");
         }
+		// Запрашиваем у пользователя уровень Яркости
+		Console.Write("Введите уровень яркости (0-100): ");
+		if (int.TryParse(Console.ReadLine(), out int light) && light >= 0 && light <= 100)
+		{
+			// Создаем команду для изменения громкости
+			var setLightCommand = new SetLightCommand(television, light);
+
+			// Создаем пульт и назначаем команду
+			remoteControl.SetCommand(setLightCommand);
+
+			// Выполняем команду
+			remoteControl.PressButton(); // Громкость устанавливается на введенное значение
+		}
+		else
+		{
+			Console.WriteLine("Некорректный ввод. Пожалуйста, введите число от 0 до 100.");
+		}
+        Console.ReadLine();
     }
 }
